6957618e31098fb4b69a80dcd0fbeaa3699c2917
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-04-21 11:46:33 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240421/두 사람/two-person.cpp
+++ 240421/두 사람/two-person.cpp
@@ -6,7 +6,7 @@ int main() {
 	char aa, bb;
 	cin >> a>>aa >>b>>bb;
 
-	if (a >= 19 || b >= 19 && aa == 'M' || bb == 'M')
+	if (a >= 19 && aa == 'M' || b >= 19 && bb == 'M')
 		cout << 1 << endl;
 	else cout << 0 << endl;
 
