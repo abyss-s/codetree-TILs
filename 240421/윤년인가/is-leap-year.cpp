7e967d46af66c5ce3faf960955681da8e910245e
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-04-21 11:57:20 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240421/윤년인가/is-leap-year.cpp
+++ 240421/윤년인가/is-leap-year.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
 	int y;
 	cin >> y;
-	if (y % 4 == 0 || y % 100 == 0 && y % 400 != 0)
-		cout << "true";
-	else cout << "false";
+	if (y % 4 != 0 || y % 100 == 0 && y % 400 != 0)
+		cout << "false";
+	else if (y % 4 == 0) cout << "true";
 	return 0;
 }
