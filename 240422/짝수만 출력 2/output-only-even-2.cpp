492e4c11ca4dcd5c94765371a2b755364e0761dd
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-04-22 21:48:56 +0900

    Title: 짝수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240422/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b;
+	cin >> b >> a;
+	int i = b;
+	while (i >= a) {
+		if (i % 2 == 0) {
+			cout << i << " ";
+		}
+		i--;
+	}
+	return 0;
+}
