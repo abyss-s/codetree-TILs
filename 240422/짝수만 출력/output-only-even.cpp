73263929e6563071573b1c1989a40d41405d24f0
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-04-22 21:39:26 +0900

    Title: 짝수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240422/짝수만 출력/output-only-even.cpp
+++ 240422/짝수만 출력/output-only-even.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 int main() {
 	int a,b;
-	cin >> a>>b;
-	for (int i = a; i <= b; i++) {
-		if (i%2==0) {
+	cin >> a >> b;
+	int i = a;
+	while (i <= b) {
+		if (i % 2 == 0) {
 			cout << i << " ";
 		}
+		i++;
 	}
 	return 0;
 }
