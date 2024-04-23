67b5f60ba1f6c31ffd4b07f2ae218e726d25910d
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-04-23 21:01:39 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240423/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+string print() {
+	string str = "**********";
+	return str;
+}
+
+int main() {
+	cout << print() << endl;
+	cout << print() << endl;
+	cout << print() << endl;
+	cout << print() << endl;
+	cout << print() << endl;
+	return 0;                                 
+}
