98960c961f3744e9e5b8c32542e3b36352927676
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-04-23 19:55:47 +0900

    Title: 배수의 개수 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240423/배수의 개수 2/number-of-multipliers-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int cnt=0;
+	for (int i = 0; i < 10; i++) {
+		int a;
+		cin >> a;
+		if (a % 2 ==1) cnt++;
+	}
+	cout << cnt;
+	return 0;
+}
