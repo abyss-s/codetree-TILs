830ac898981412d2dce1d139c61bfc6a983b18d4
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-04-23 20:00:45 +0900

    Title: 등장횟수 확인, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240423/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int cnt = 0;
+	for (int i = 0; i < 5; i++) {
+		int a;
+		cin >> a;
+		if (a % 2 == 0) cnt++;
+	}
+	cout << cnt;
+	return 0;
+}
