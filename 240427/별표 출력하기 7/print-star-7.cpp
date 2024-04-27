05b47eec0753b5a1855016b3d6c5be3ca666e50d
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-04-27 17:00:12 +0900

    Title: 별표 출력하기 7, Time: 3ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240427/별표 출력하기 7/print-star-7.cpp
+++ 240427/별표 출력하기 7/print-star-7.cpp
@@ -1,19 +1,6 @@
 #include <iostream>
-#include <string>
-using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	int n;
-	cin >> n;
-	for (int i = 0; i < n ; i++) {
-		for (int j = 0; j <= i; j++) {
-			cout << "*" << " ";
-		}
-		cout << endl;
-	}
-	return 0;
+    // 여기에 코드를 작성해주세요.
+    return 0;
 }
