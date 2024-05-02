57a0344c8eac86b58a4e93ebef49f885d48241e3
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-05-03 01:36:03 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240503/출석 부르기/calling-attendance.cpp
+++ 240503/출석 부르기/calling-attendance.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    return 0;
+  int n;
+  cin >> n;
+  if (n == 1) {
+    cout << "John" << endl;
+  } else if (n == 2) {
+    cout << "Tom" << endl;
+  } else if (n == 3) {
+    cout << "Paul" << endl;
+  } else
+    cout << "Vacancy" << endl;
+
+  return 0;
 }
