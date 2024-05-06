be062a7b59278c45f7d5b549a81a182e754cfc91
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-05-07 02:07:54 +0900

    Title: 2021년 날짜의 유무, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240507/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int whatDay(int m) {
+  if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
+    return 31;
+  else if (m == 4 || m == 6 || m == 9 || m == 11)
+    return 30;
+  else if (m == 2)
+    return 28;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int a, b;
+  cin >> a >> b;
+
+  if (b <= whatDay(a)) {
+    cout << "Yes";
+  } else {
+    cout << "No";
+  }
+
+  return 0;
+}
