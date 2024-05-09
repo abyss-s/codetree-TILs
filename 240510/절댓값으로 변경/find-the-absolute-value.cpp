be7de9b1a6dacec126a5714c6b0c7a987ae2f612
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-05-10 01:13:38 +0900

    Title: 절댓값으로 변경, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240510/절댓값으로 변경/find-the-absolute-value.cpp
+++ 240510/절댓값으로 변경/find-the-absolute-value.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    return 0;
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int n;
+  int arr[100];
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+  for (int i = 0; i < n; i++) {
+    if (arr[i] < 0)
+      cout << -arr[i];
+    else
+      cout << arr[i];
+    cout << " ";
+  }
+  return 0;
 }
