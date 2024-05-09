8fab8c507e3acb21eb566a063c2ed2cc199b92f2
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-05-09 16:20:09 +0900

    Title: 짝수만 2로 나누기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240509/짝수만 2로 나누기/divide-even-numbers-by-2.cpp
+++ 240509/짝수만 2로 나누기/divide-even-numbers-by-2.cpp
@@ -6,19 +6,17 @@ int main() {
   cin.tie(NULL);
   cout.tie(NULL);
 
-  while (true) {
-    int n=0;
-    int arr[100];
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-      cin >> arr[i];
-    }
-    for (int i = 0; i < n; i++) {
-      if (arr[i] % 2 == 0)
-        cout << arr[i] / 2 << " ";
-      else
-        cout << arr[i] << " ";
-    }
+  int n = 0;
+  int arr[100];
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+  for (int i = 0; i < n; i++) {
+    if (arr[i] % 2 == 0)
+      cout << arr[i] / 2 << " ";
+    else
+      cout << arr[i] << " ";
   }
 
   return 0;
