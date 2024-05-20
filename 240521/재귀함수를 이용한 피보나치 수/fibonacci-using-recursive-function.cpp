2a90bd4f9f58f24ab45a12daa875337ce4ec7534
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-05-21 01:29:53 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240521/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int F(int n) {
+  if (n == 1)
+    return 1;
+  if (n == 2)
+    return 1;
+  return F(n - 1) + F(n - 2);
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int n;
+  cin >> n;
+  cout << F(n);
+
+  return 0;
+}
