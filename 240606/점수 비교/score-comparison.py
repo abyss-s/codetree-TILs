6901d04d912f1ffc46b795e04599fb0eac5d9949
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-06-06 16:33:52 +0900

    Title: 점수 비교, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240606/점수 비교/score-comparison.py
@@ -0,0 +1,10 @@
+str1 = input().split(" ")
+a = int(str1[0])
+b = int(str1[1])
+str2 = input().split(" ")
+aa = int(str2[0])
+bb = int(str2[1])
+if a > b == 0 and aa > bb:
+    print(1)
+else:
+    print(0)
