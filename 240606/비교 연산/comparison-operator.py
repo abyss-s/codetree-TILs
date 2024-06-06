0941a5d3449c9ddf98abb2475aa918bd29c1f0c1
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-06-06 16:27:32 +0900

    Title: 비교 연산, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240606/비교 연산/comparison-operator.py
@@ -0,0 +1,27 @@
+str = input().split(" ")
+a = int(str[0])
+b = int(str[1])
+if a >= b:
+    print(1)
+else:
+    print(0)
+if a > b:
+    print(1)
+else:
+    print(0)
+if a <= b:
+    print(1)
+else:
+    print(0)
+if a < b:
+    print(1)
+else:
+    print(0)
+if a == b:
+    print(1)
+else:
+    print(0)
+if a != b:
+    print(1)
+else:
+    print(0)
