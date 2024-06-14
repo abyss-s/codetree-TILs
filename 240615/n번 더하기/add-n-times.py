3a0da3009bcf6fee2f51e8f4814da34ec8cb2b0f
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-06-15 02:44:35 +0900

    Title: n번 더하기, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240615/n번 더하기/add-n-times.py
+++ 240615/n번 더하기/add-n-times.py
@@ -1,8 +1,6 @@
 input_str = input().split(" ")
 a, n = int(input_str[0]), int(input_str[1])
 
-result = a + n
-
-for i in range(a + 1):
-    print(result)
-    result += n
+for _ in range(n):
+    a += n
+    print(a)
