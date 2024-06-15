3abd7ed8250fa3f7d7b0b2378f51d139ebbc8542
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-06-15 16:59:32 +0900

    Title: 특정 구간의 합, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240615/특정 구간의 합/sum-of-specific-intervals.py
+++ 240615/특정 구간의 합/sum-of-specific-intervals.py
@@ -1,9 +1,9 @@
 nm = input().split()
 n, m = int(nm[0]), int(nm[1])
-arr = list(map(int, input().split(" ")))
+arr = list(map(int, input().split()))
 sum_arr = []
 for _ in range(m):
-    a, b = map(int, input().split(" "))
+    a, b = map(int, input().split())
     sum_arr.append(sum(arr[a - 1 : b]))
 for s in sum_arr:
     print(s)
