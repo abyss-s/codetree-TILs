79db75e2c7bc12c87be8188f7cbf1ddc5c41e377
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-06-15 15:33:25 +0900

    Title: 369 게임, Time: 150ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240615/369 게임/369-game.py
@@ -0,0 +1,11 @@
+n = int(input())
+for i in range(1, n + 1):
+    if (
+        i % 3 == 0
+        or str(i).count("3") >= 1
+        or str(i).count("6") >= 1
+        or str(i).count("9") >= 1
+    ):
+        print(0, end=" ")
+    else:
+        print(i, end=" ")
