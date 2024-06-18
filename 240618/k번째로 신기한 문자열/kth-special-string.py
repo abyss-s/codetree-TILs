e12002a53b9f1a3062b01f8b00c5152c0f2f74c4
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-06-18 17:28:37 +0900

    Title: k번째로 신기한 문자열, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240618/k번째로 신기한 문자열/kth-special-string.py
+++ 240618/k번째로 신기한 문자열/kth-special-string.py
@@ -1,11 +1,10 @@
 s = input().split(" ")
-n, k, T = int(s[0]), int(s[1]), str(s[2])
+n, k, T = int(s[0]), int(s[1]), s[2]
 
 str_arr = []
-for i in range(n):
+for _ in range(n):
     str_i = input()
-    if T in str_i:
+    if str_i.startswith((T)):
         str_arr.append(str_i)
 str_arr.sort()
-
 print(str_arr[k - 1])
