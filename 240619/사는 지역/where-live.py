947a76882de9f0666a03605331ca126e702d0acf
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-06-19 16:49:04 +0900

    Title: 사는 지역, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240619/사는 지역/where-live.py
@@ -0,0 +1,17 @@
+class Person:
+    def __init__(self, name, addr, city):
+        self.name = name
+        self.addr = addr
+        self.city = city
+
+
+n = int(input())
+arr = []
+for i in range(n):
+    input_str = input().split(" ")
+    arr.append(Person(input_str[0], input_str[1], input_str[2]))
+sorted_arr = sorted(arr, key=lambda person: person.name)
+first_person = sorted_arr[2]
+print(f"name {first_person.name}")
+print(f"addr {first_person.addr}")
+print(f"city {first_person.city}")
