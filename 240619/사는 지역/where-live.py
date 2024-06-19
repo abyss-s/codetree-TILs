5cad4529c7bccaeda0dad563b08b1db157fc48d9
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-06-19 16:49:39 +0900

    Title: 사는 지역, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 240619/사는 지역/where-live.py
+++ 240619/사는 지역/where-live.py
@@ -11,7 +11,7 @@ for i in range(n):
     input_str = input().split(" ")
     arr.append(Person(input_str[0], input_str[1], input_str[2]))
 sorted_arr = sorted(arr, key=lambda person: person.name)
-first_person = sorted_arr[2]
+first_person = sorted_arr[n - 1]
 print(f"name {first_person.name}")
 print(f"addr {first_person.addr}")
 print(f"city {first_person.city}")
