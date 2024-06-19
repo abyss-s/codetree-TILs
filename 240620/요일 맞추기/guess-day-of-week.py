92649ac208b4e07deac23da26550c313e7dea230
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-06-20 00:43:12 +0900

    Title: 요일 맞추기, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240620/요일 맞추기/guess-day-of-week.py
+++ 240620/요일 맞추기/guess-day-of-week.py
@@ -2,13 +2,19 @@ m1, d1, m2, d2 = tuple(map(int, input().split()))
 days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 str_day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
-sum = 1
-if m1 == m2:
-    sum = d2 - d1 + 1
-else:
-    sum += days[m1] - d1
-    for m in range(m1 + 1, m2):
-        sum += days[m]
-    sum += d2
+total_days = 1
 
-print(str_day[sum % 7])
+if m1 == m2:
+    total_days = d2 - d1 + 1
+elif m1 < m2:
+    total_days += days[m1] - d1
+    for m in range(m1 + 1, m2):
+        total_days += days[m]
+    total_days += d2
+else:
+    total_days += days[m1] - d1
+    for m in range(m2, m1):
+        total_days += days[m]
+    total_days += d2
+
+print(str_day[total_days % 7])
