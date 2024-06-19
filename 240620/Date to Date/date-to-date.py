f25a6ec0a8e133fdc5c5d83f3c9eca9be539ff28
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-06-20 00:11:23 +0900

    Title: Date to Date, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 240620/Date to Date/date-to-date.py
+++ 240620/Date to Date/date-to-date.py
@@ -1,15 +1,16 @@
 n = input().split(" ")
 m1, d1, m2, d2 = int(n[0]), int(n[1]), int(n[2]), int(n[3])
 days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-sum = 1
+sum_days = 1
 
 # 같은 달일 경우
 if m1 == m2:
-    sum = d2 - d1 + 1
+    sum_days = d2 - d1 + 1
 # 다른 달일 경우
 else:
-    sum += days[m1] - d1  # m1의 일수
-    for m in range(m1, m2 - 1):  # 사이에 있는 월의 총 일수
-        sum += days[m]
-    sum += d2  # m2의 일수
-print(sum)
+    sum_days += days[m1] - d1  # m1의 남은 일수
+    for m in range(m1 + 1, m2):  # 사이에 있는 월의 총 일수
+        sum_days += days[m]
+    sum_days += d2  # m2의 일수
+
+print(sum_days)
