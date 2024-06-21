47fdafbae3d8e7668f576f3726bd0271d9f8cba7
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-06-21 16:31:35 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240621/T를 초과하는 연속 부분 수열/subsequence-above-t.py
+++ 240621/T를 초과하는 연속 부분 수열/subsequence-above-t.py
@@ -1,19 +1,17 @@
-n, t = map(int, input().split())
-arr = list(map(int, input().split()))
+import sys
+
+n, t = map(int, sys.stdin.readline().split())
+arr = list(map(int, sys.stdin.readline().split()))
 
 cnt = 0
 max_cnt = 0
 
-if n == 1:
-    print(0)
-else:
-    cnt = 0
-    max_cnt = 0
-    for i in range(1, n):
-        if t < arr[i - 1] < arr[i]:
-            cnt += 1
-        else:
-            max_cnt = max(max_cnt, cnt)
-            cnt = 1
-    max_cnt = max(max_cnt, cnt)
-    print(max_cnt if max_cnt >= 1 else 0)
+for i in range(n - 1):
+    if arr[i] > t:
+        cnt += 1
+    else:
+        max_cnt = max(max_cnt, cnt)
+        cnt = 0
+
+max_cnt = max(max_cnt, cnt)
+print(max_cnt if max_cnt > 0 else 0)
