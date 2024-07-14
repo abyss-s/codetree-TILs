b90e11184d3fce7e61645d7ed4c8286df2843b3b
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-07-14 22:54:23 +0900

    Title: 동전이 있는 위치, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240714/동전이 있는 위치/Where-coins-are-located.py
@@ -0,0 +1,12 @@
+a, b = 1, 3
+n, m = map(int, input().split())  # n = 격자의 크기, m = 동전 개수
+arr_2d = [[0 for _ in range(n)] for _ in range(n)]  # n*n의 격자 2차원 배열 생성
+for _ in range(m):
+    r, c = tuple(map(int, input().split()))
+    arr_2d[r - 1][c - 1] = 1
+
+
+for row in arr_2d:
+    for elem in row:
+        print(elem, end=" ")
+    print()
