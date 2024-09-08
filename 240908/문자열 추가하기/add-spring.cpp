4cd2bf93922c87f2efc0ad6f3274a07892873433
Youngju <77565980+abyss-s@users.noreply.github.com>
2024-09-08 23:14:21 +0900

    Title: 문자열 추가하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240908/문자열 추가하기/add-spring.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+  // 실행속도 높이기
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  string str;
+  cin >> str;
+  cout << str << "Hello";
+
+  return 0;
+}
