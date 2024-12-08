d724c688e2524aad19ba07ea47d00c5c49531a0e
Lee Youngju <77565980+abyss-s@users.noreply.github.com>
2024-12-08 16:03:50 +0900

    Title: 제곱수의 합으로 나타내기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241208/제곱수의 합으로 나타내기/express-as-sum-of-squares.cpp
+++ 241208/제곱수의 합으로 나타내기/express-as-sum-of-squares.cpp
@@ -1,5 +1,4 @@
 #include <algorithm>
-#include <deque>
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -7,8 +6,8 @@ using namespace std;
 int sol(int n) {
   vector<int> dp(n + 1, 5000);
   dp[0] = 0;
-  for (int i = 1; i <= n; i++) {
-    for (int j = 1; j * j <= i; j++) {
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j * j <= i; ++j) {
       dp[i] = min(dp[i], dp[i - j * j] + 1);
     }
   }
@@ -18,7 +17,6 @@ int sol(int n) {
 int main() {
   int n;
   cin >> n;
-  cout << sol(n);
-
+  cout << sol(n) << endl;
   return 0;
 }
