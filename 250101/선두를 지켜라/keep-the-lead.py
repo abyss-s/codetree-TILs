a4d56ca7c8f0140b604d22afc6b6523bbc55227b
Lee Youngju <77565980+abyss-s@users.noreply.github.com>
2025-01-01 23:57:38 +0900

    Title: 선두를 지켜라, Time: 60ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250101/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,36 @@
+n, m = map(int, input().split())
+
+# 선두 변경 횟수
+res = 1
+# 이동 기록 저장
+a_moves, b_moves = [], []
+# 이동 거리
+a_dist, b_dist = 0, 0
+
+for i in range(n):
+    v, t = map(int, input().split())
+    a_moves.extend([v] * t)
+
+for i in range(m):
+    v, t = map(int, input().split())
+    b_moves.extend([v] * t)
+
+prev = 0
+
+for i in range(n):
+    a_dist += a_moves[i]
+    b_dist += b_moves[i]
+
+    if a_dist > b_dist:
+        cur = -1
+    elif a_dist < b_dist:
+        cur = 1
+    else:
+        cur = 0
+
+    if cur != prev and cur != 0:
+        res += 1
+
+    prev = cur
+
+print(res)
