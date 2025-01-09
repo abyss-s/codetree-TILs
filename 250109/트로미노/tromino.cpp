c508c0655cc6d8742982f9c4dad98517024146d0
Lee Youngju <77565980+abyss-s@users.noreply.github.com>
2025-01-09 23:51:23 +0900

    Title: 트로미노, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

--- 250109/트로미노/tromino.cpp
+++ 250109/트로미노/tromino.cpp
@@ -1,54 +1,77 @@
-#include <algorithm>
 #include <iostream>
+#include <algorithm>
+
+#define MAX_NUM 200
+
 using namespace std;
 
 int n, m;
-int grid[200][200];
+int grid[MAX_NUM][MAX_NUM];
 
-// 트로미노 블록 (상대 좌표)
-int tromino[4][3][2] = {
-    {{0, 0}, {0, 1}, {0, 2}},  // 가로
-    {{0, 0}, {1, 0}, {2, 0}},  // 세로
-    {{0, 0}, {1, 0}, {1, 1}},  // ㄱ
-    {{0, 1}, {1, 0}, {1, 1}}   // ㄴ
+// 가능한 모든 모양을 전부 적어줍니다.
+int shapes[6][3][3] = {
+    {{1, 1, 0},
+    {1, 0, 0},
+    {0, 0, 0}},
+
+    {{1, 1, 0},
+    {0, 1, 0},
+    {0, 0, 0}},
+
+    {{1, 0, 0},
+    {1, 1, 0},
+    {0, 0, 0}},
+
+    {{0, 1, 0},
+    {1, 1, 0},
+    {0, 0, 0}},
+
+    {{1, 1, 1},
+    {0, 0, 0},
+    {0, 0, 0}},
+
+    {{1, 0, 0},
+    {1, 0, 0},
+    {1, 0, 0}},
 };
 
-int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-
-  cin >> n >> m;
-
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      cin >> grid[i][j];
-    }
-  }
-
-  int maxSum = 0;
-
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      for (int t = 0; t < 4; t++) {
+// 주어진 위치에 대하여 가능한 모든 모양을 탐색하며 최대 합을 반환합니다.
+int GetMaxSum(int x, int y) {
+    int max_sum = 0;
+    
+    for(int i = 0; i < 6; i++) {
+        bool is_possible = true;
         int sum = 0;
-        bool valid = true;
-        for (int k = 0; k < 3; k++) {
-          int ni = i + tromino[t][k][0];
-          int nj = j + tromino[t][k][1];
-          if (ni >= 0 && ni < n && nj >= 0 && nj < m) {
-            sum += grid[ni][nj];
-          } else {
-            valid = false;
-            break;
-          }
-        }
-        if (valid)
-          maxSum = max(maxSum, sum);
-      }
+        for(int dx = 0; dx < 3; dx++)
+            for(int dy = 0; dy < 3; dy++) {
+                if(shapes[i][dx][dy] == 0) continue;
+                if(x + dx >= n || y + dy >= m) is_possible = false;
+                else sum += grid[x + dx][y + dy];
+            }
+
+        if(is_possible)
+            max_sum = max(max_sum, sum);
     }
-  }
+    
+    return max_sum;
+}
 
-  cout << maxSum << endl;
-
-  return 0;
+int main() {
+	
+	cin >> n >> m;
+	
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < m; j++)
+			cin >> grid[i][j];
+	
+	int ans = 0;
+	
+    // 격자의 각 위치에 대하여 탐색하여줍니다.
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < m; j++)
+			ans = max(ans, GetMaxSum(i, j));
+	
+	cout << ans;
+	
+	return 0;
 }
