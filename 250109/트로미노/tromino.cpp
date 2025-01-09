9575b28553389fbcf68d6d0997cf0e034c375cdd
Lee Youngju <77565980+abyss-s@users.noreply.github.com>
2025-01-09 23:47:34 +0900

    Title: 트로미노, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250109/트로미노/tromino.cpp
+++ 250109/트로미노/tromino.cpp
@@ -17,27 +17,31 @@ int main() {
     }
   }
 
-  // Write your code here!
   int maxSum = 0;
 
-  // 니은 블럭
-  for (int i = 0; i < n - 2; i++) {
-    for (int j = 0; j < m - 1; j++) {
-      int sum =
-          grid[i][j] + grid[i + 1][j] + grid[i + 2][j] + grid[i + 2][j + 1];
-      maxSum = max(sum, maxSum);
-    }
-  }
-
-  // 일자 블럭
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m - 3; j++) {
-      int sum = grid[i][j] + grid[i][j + 1] + grid[i][j + 2] + grid[i][j + 3];
+    for (int j = 0; j < m - 2; j++) {
+      int sum = grid[i][j] + grid[i][j + 1] + grid[i][j + 2];
       maxSum = max(maxSum, sum);
     }
   }
 
-  cout << maxSum;
+  for (int i = 0; i < n - 2; i++) {
+    for (int j = 0; j < m; j++) {
+      int sum = grid[i][j] + grid[i + 1][j] + grid[i + 2][j];
+      maxSum = max(maxSum, sum);
+    }
+  }
+
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < m - 1; j++) {
+      int sum1 = grid[i][j] + grid[i + 1][j] + grid[i + 1][j + 1];
+      int sum2 = grid[i][j + 1] + grid[i + 1][j] + grid[i + 1][j + 1];
+      maxSum = max({maxSum, sum1, sum2});
+    }
+  }
+
+  cout << maxSum << endl;
 
   return 0;
 }
